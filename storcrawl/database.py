c0586eeaed384ed343a7cce0f340090f3d5e5b5b
Dan Tenenbaum <dtenenba@fhcrc.org>
2017-05-05 11:25:19 -0700

    remove storcrawl directory, contents have moved to another repo

--- storcrawl/database.py
+++
@@ -1,7 +0,0 @@
-"""
-Database goes in a separate file to avoid circular dependencies.
-"""
-
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy() # pylint: disable=invalid-name
--- storcrawl/start_app.py
+++
@@ -1,6 +0,0 @@
-"""
-Entry point for gunicorn
-"""
-from app import create_app
-
-app = create_app() # pylint: disable=invalid-name
